8872881256b3558af2b8c31d22030c3e9a13d48e
MoonLight <skymoon9406@gmail.com>
2018-05-20 14:50:05 +0800

    Add files via upload

---
+++ 03.DangDangBooks/DangDang/items.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DangdangItem(scrapy.Item):
+    # define the fields for your item here like:
+    title = scrapy.Field()
+    link = scrapy.Field()
+    Comment_Num = scrapy.Field()
+    price = scrapy.Field()
---
+++ 03.DangDangBooks/DangDang/pipelines.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import pymysql
+
+
+
+
+
+class DangdangPipeline(object):
+    def process_item(self, item, spider):
+        # ========================================================================================
+        # Mysql connect
+        conn = pymysql.connect(host="127.0.0.1",
+                               user="root",
+                               password="yeshenzn,./123.",
+                               db="dangdang",
+                               charset="utf8")
+        cur = conn.cursor()
+        # ========================================================================================
+        for i in range(len(item['title'])):
+            title = item["title"][i]
+            Comment_Num = item["Comment_Num"][i][:-3]
+            price = item["price"][i][1:]
+            link = item["link"][i]
+            sql = "INSERT INTO books(title,link,comment_num,price) VALUES ('"+title+"','"+link+"','"+Comment_Num+"','"+price+"')"
+            sta = cur.execute(sql)
+            conn.commit()
+        cur.close()
+        conn.close()
+        return item
